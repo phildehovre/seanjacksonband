67214525380c9353789f8e420b602b7a9fb13bc2
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-08-16 16:40:45 +0100

    emailjs working

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,25 +2,54 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { formSchema } from "@/lib/Constants";
 import { zodResolver } from "@hookform/resolvers/zod";
+import emailjs from "@emailjs/browser";
+
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "./ui/input";
 import Section from "./Section";
 import { Button } from "./ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useRef } from "react";
 
 function Contact() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
-      email: "",
+      from_firstName: "",
+      from_lastName: "",
+      user_email: "",
       message: "",
     },
   });
 
+  const formRef = useRef<HTMLFormElement | null>(null);
+
   const isLoading = form.formState.isSubmitting;
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_w9z916m",
+        "template_momqrgr",
+        formRef.current as unknown as HTMLFormElement,
+        // data as HTMLFormElement,
+        import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     console.log(data);
+    sendEmail(data);
   };
 
   return (
@@ -29,12 +58,13 @@ function Contact() {
       <div>
         <Form {...form}>
           <form
+            ref={formRef}
             onSubmit={form.handleSubmit(onSubmit)}
             className="rounded-lg border w-full p-4 px-3 md:px-6
           focus-within:shadow-sm grid grid-cols-12 gap-2"
           >
             <FormField
-              name="name"
+              name="from_firstName"
               render={({ field }) => (
                 <FormItem className="col-span-12 lg:col-span-10">
                   <FormControl className="m-0 p-0">
@@ -49,7 +79,22 @@ function Contact() {
               )}
             />
             <FormField
-              name="email"
+              name="from_lastName"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="text-blue-600 border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading}
+                      placeholder="Name"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="user_email"
               render={({ field }) => (
                 <FormItem className="col-span-12 lg:col-span-10">
                   <FormControl className="m-0 p-0">
@@ -68,7 +113,7 @@ function Contact() {
               render={({ field }) => (
                 <FormItem className="col-span-12 lg:col-span-10">
                   <FormControl className="m-0 p-0">
-                    <Input
+                    <Textarea
                       className="text-blue-600 border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                       disabled={isLoading}
                       placeholder="Your message"
@@ -82,6 +127,7 @@ function Contact() {
               className="col-span-12 lg:col-span-2 w-full"
               disabled={isLoading}
               type="submit"
+              onClick={sendEmail}
             >
               Send!
             </Button>
--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,7 +1,8 @@
 import * as z from "zod"
 
 export const formSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    email: z.string().email("Invalid email address"),
+    from_firstName: z.string().min(1, "First name is required"),
+    from_lastName: z.string().min(1, "Last name is required"),
+    user_email: z.string().email("Invalid email address"),
     message: z.string().min(1, "Message is required"),
 })
