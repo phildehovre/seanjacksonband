93b2d35537440ab1eac8f4169191f74f4f0a592d
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-08-16 21:19:15 +0100

    Hero styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import { useState } from "react";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
