a4840399d399da589fe36198851d0d5a30d7b1b5
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-08-26 14:44:29 +0100

    logo v1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,13 +27,16 @@ function Hero() {
     <section className="hero" id="top">
       <div className="background-img"></div>
       <span className="slogan-ctn" id="slogan">
-        <span>The</span>
+        {/* <span>The</span>
         <h1>
           Sean
           <br /> Jackson
         </h1>
-        <span>Band</span>
-        <p>Your Ultimate Country Blues Rock Wedding Experience</p>
+        <span>Band</span> */}
+        <img src="src\assets\seanjacksonband-logo.png" />
+        <p className="text-2xl py-5">
+          Your Ultimate Country Blues Rock Wedding Experience
+        </p>
         <span className="flex justify-center md:justify-start">
           <a href="#contact" className="cta-button">
             Book us!
