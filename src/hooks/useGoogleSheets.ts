d12e1f1a999e76d0a1da2aeb0f528c4722253e28
Philippe De Hovre <ph.dehovre@gmail.com>
2025-09-20 09:06:05 +0200

    fix mobile menu

--- src/hooks/useGoogleSheets.ts
+++ src/hooks/useGoogleSheets.ts
@@ -28,7 +28,6 @@ export function useGoogleSheet<T = any>(csvUrl: string) {
 					}, {} as Record<string, string>)
 				);
 
-				console.log(parsed);
 				setData(parsed as T[]);
 			} catch (err: any) {
 				setError(err.message);
